6d2dbee6c73fd07f2603c3a7a8249414e23c8296
Philip Kristian Møller Flyvholm <phif@itu.dk>
2022-10-16 17:06:25 +0200

    Deployment to github

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -5,5 +5,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://philipflyvholm.github.io',
+  base: '/estree-explorer',
   integrations: [svelte()]
 });
